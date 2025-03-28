4cc6a12090ff6ff9fd47e149bb2418739cd74d44
kburd <kalebmburd@gmail.com>
2025-03-28 13:59:00 -0400

    fixing header and adding netlify deploy scripts

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,25 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { PAGES } from '../App';
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 
 const Header = () => {
 
     const location = useLocation();
-    const [activeTab, setActiveTab] = useState()
-
-    useEffect(() => {
-        if (PAGES.filter(page => page.path == location.pathname).length === 1){
-            setActiveTab(PAGES.filter(page => page.path == location.pathname)[0].index)
-        }
-    }, [location]);
 
     return (
         <header id="header" class="fixed-top header-scrolled">
-            <div class="container d-flex align-items-center justify-content-between">
-                <h1 class="logo"><a href="/">FactorialYou</a></h1>
-
-                <nav id="navbar" class="navbar">
-                    <ul>
-                        {PAGES.filter(page => page.isTab).map(page => {
-                            let className = (activeTab === page.index) ? "nav-link active" : "nav-link"
-                            return <li><a className={className} href={page.path}>{page.label}</a></li>
-                        })}
-                    </ul>
-                    <i class="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-
-            </div>
+            <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="/" className="logo">Factorial You</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto" activeKey={location.pathname}>
+                            {PAGES.filter(page => page.isTab).map(page => <Nav.Link href={page.path}>{page.label}</Nav.Link>)}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 };
